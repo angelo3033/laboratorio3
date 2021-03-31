225e6763e1d5c901a9547651371bbd8a2b5eda1a
angel samanez <angel.samanez@tecsup.edu.pe>
2021-03-31 15:45:32 -0500

    Amplifique estadisticas

--- proyecto1/src/index.js
+++ proyecto1/src/index.js
@@ -6,17 +6,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const ClickGood = () => {
     setGood(good+1)
+    setAll(all+1)
+    setAverage(average+0.1)
+    setPositive(((good+1)*100)/(all+1))
   }
 
   const ClickNeutral = () => {
     setNeutral(neutral+1)
+    setAll(all+1)
+    setAverage(average+0)
+    setPositive(((good)*100)/(all+1))
   }
 
   const ClickBad = () => {
     setBad(bad+1)
+    setAll(all+1)
+    setAverage(average-(1/22.5))
+    setPositive(((good)*100)/(all+1))
   }
 
   return (
@@ -32,6 +44,9 @@ const App = () => {
         <p>good: {good}</p>
         <p>neutral: {neutral}</p>
         <p>bad: {bad}</p>
+        <p>all: {all}</p>
+        <p>average: {average}</p>
+        <p>positive: {positive} %</p>
       </div>
     </div>
     
