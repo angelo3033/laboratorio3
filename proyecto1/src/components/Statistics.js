611ff8b6c453b2593d5dff48e9b5762d253989a6
angel samanez <angel.samanez@tecsup.edu.pe>
2021-03-31 17:22:33 -0500

    conditional render

--- proyecto1/src/components/Statistics.js
+++ proyecto1/src/components/Statistics.js
@@ -1,13 +1,22 @@
 const Statistics = (props) => {
+
+    let {good, neutral, bad} = props
+
     return (
         <div>
             <h1>Statistics</h1>
-            <p>good: {props.good}</p>
-            <p>neutral: {props.neutral}</p>
-            <p>bad: {props.bad}</p>
-            <p>all: {props.all}</p>
-            <p>average: {props.average}</p>
-            <p>positive: {props.positive} %</p>
+            {good || neutral || bad ?
+            <div>
+                <p>good: {good}</p>
+                <p>neutral: {neutral}</p>
+                <p>bad: {bad}</p>
+                <p>all: {props.all}</p>
+                <p>average: {props.average}</p>
+                <p>positive: {props.positive} %</p>
+            </div>
+            :
+                <h4>No feedback given</h4>
+            }  
         </div>
     )
 }
